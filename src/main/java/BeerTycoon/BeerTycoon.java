98c2f9638f95197c14a50109cc46666e34b789a9
Mitch Kubina <mtkubina6@gmail.com>
2025-11-22 17:11:26 -0700

    Just playing around rn

--- src/main/java/BeerTycoon/BeerTycoon.java
+++ src/main/java/BeerTycoon/BeerTycoon.java
@@ -1,4 +1,31 @@
 package BeerTycoon;
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class BeerTycoon {
+
+    public BeerTycoon(String title) {
+        JFrame frame = new JFrame(title);
+        frame.setSize(400,300);
+
+        JButton button = new JButton("button");
+        button.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("button pressed");
+            }
+        });
+
+        frame.add(button);
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        BeerTycoon game = new BeerTycoon("poop");
+    }
+
 }
