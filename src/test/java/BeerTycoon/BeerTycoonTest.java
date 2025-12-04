6a282f5728d7d9fca1eec6b127388c561dd34d8d
Eduardo-Zone <Cameron.maynor@gmail.com>
2025-12-04 15:29:27 -0700

    partways making first test for BeerTycoon Instance

--- src/test/java/BeerTycoon/BeerTycoonTest.java
+++ src/test/java/BeerTycoon/BeerTycoonTest.java
@@ -1,6 +1,10 @@
 package BeerTycoon;
 
+import BeerTycoon.BeerMakers.BeerMakerFactory;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -8,7 +12,10 @@ public class BeerTycoonTest {
 
     @Test
     void testInstance() {
-        BeerTycoon game = new BeerTycoon("Beer Tycoon");
+        BeerMakerFactory factory = new BeerMakerFactory();
+        List<String> buttons = new ArrayList<>();
+        buttons.add("Make Beer");
+        BeerTycoon game = new BeerTycoon(factory, buttons);
 
         assertTrue(true);
     }
